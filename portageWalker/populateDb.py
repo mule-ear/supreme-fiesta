6c4e46feb8b7e02efd5e86cbd4c943e3d266140f
mule-ear <mule-ear@gmail.com>
2018-01-23 22:22:18 -0700

    It populates the packages table now, plus a nifty progress bar I found on SO

--- portageWalker/populateDb.py
+++ portageWalker/populateDb.py
@@ -1,7 +1,29 @@
 import sqlite3, os
 import include.portageWalker as PW
 
-def populateTLD(c, pwDict):
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    ## https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+def populateTLD(conn, c, pwDict):
     c.execute("DROP TABLE IF EXISTS tld;")
     c.execute("CREATE TABLE tld(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dir_name VARCHAR , description STRING);")
 
@@ -15,28 +37,33 @@ def populateTLD(c, pwDict):
 
     conn.commit()
 
-def getDescription(pDir, pName):
-    path = pDir + pName + '/'
+def getDescription(packageDir, packageName):
+    path = packageDir + packageName + '/'
     
     # search in path for a file called pname*ebuild
     for entry in os.listdir(path):
-        #print(entry)
-        print(os.path.join(path, entry),pName)
-        if (os.path.isfile(os.path.join(path, entry)) and (pName in entry) and ('ebuild' in entry)) :
-            f = open(path+ '/' + entry, "r")
-            g = f.read()
-            print(g)
-            if "DESCRIPTION" in g:
-                print (g)
-                return g.split("DESCRIPTION: ")[1]
+        if (os.path.isfile(os.path.join(path, entry)) and (packageName in entry) and ('ebuild' in entry)) :
+            #print(os.path.join(path,entry), packageName, entry)
+            with open(path + entry, "r") as f:
+                # There's gotto be a cleaner way to do this
+                g = f.readlines()
+                for line in g:
+                    if "DESCRIPTION" in line:
+                        f.close()
+                        return line.split("DESCRIPTION=")[1]
     
 
-def populatePackages(c, portDir):
+def populatePackages(conn, c, portDir):
     listOfPackages = []
 
     # Create the packages table
     c.execute("DROP TABLE IF EXISTS packages;")
     c.execute("CREATE TABLE packages(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR, parent INTEGER, description VARCHAR);")
+    
+    c.execute("SELECT COUNT(*) FROM tld")
+    count=int(c.fetchone()[0])
+    i = 0;
+    printProgressBar(0, count, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
     # put package groups into a list
     c.execute("SELECT id,dir_name FROM tld;")
@@ -51,31 +78,35 @@ def populatePackages(c, portDir):
         
         for entry in os.listdir(path):
             if (os.path.isdir(os.path.join(path, entry))):
-                #listOfPackages.append(entry)
-                print("INSERT INTO packages(name, parent) VALUES(" + entry + ","+ str(parentId) + ");")
-                c.execute("INSERT INTO packages(name, parent) VALUES('" + entry + "',"+ str(parentId) + ");")
+                description = getDescription(path,entry)
+
+                if description is not None and "'" in description:
+                    description = description.replace("'",chr(8217))
+                elif description is None:
+                    description = "None"
+                
+                c.execute("INSERT INTO packages(name,  parent, description) VALUES('" + entry + "',"+ str(parentId) + ", '"+description+"');")
             # 
         conn.commit()
+        i+=1
+        printProgressBar( i , count, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-    print(listOfPackages)
 
 if __name__ == "__main__":
     pw = PW.portageWalker()
     HOME = os.environ['HOME']+"/"
     portageDirectory = "/usr/portage/"
     packageGroupDict = pw.tldDict('en')
-    conn = sqlite3.connect(HOME + 'portwalk.db')
-    cur = conn.cursor()
+    con = sqlite3.connect(HOME + 'portwalk.db')
+    cur = con.cursor()
 
     # First - get the package groups
     # and the descriptions
-    populateTLD(cur, packageGroupDict) 
+    print("Populating package groups table...")
+    populateTLD(con, cur, packageGroupDict) 
 
     # Now populate the packages table
-    populatePackages(cur,portageDirectory)
+    print("Populatong packages table...")
+    populatePackages(con, cur, portageDirectory)
 
-
-
-
-
-conn.close()
+    con.close()
