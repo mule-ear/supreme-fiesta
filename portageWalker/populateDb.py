c05941c75d6f45b866addd8649d215fdf5291d5d
mule-ear <mule-ear@gmail.com>
2018-01-23 21:04:06 -0700

    Got the packages tables populated, less description

--- portageWalker/populateDb.py
+++ portageWalker/populateDb.py
@@ -15,13 +15,64 @@ def populateTLD(c, pwDict):
 
     conn.commit()
 
+def getDescription(pDir, pName):
+    path = pDir + pName + '/'
+    
+    # search in path for a file called pname*ebuild
+    for entry in os.listdir(path):
+        #print(entry)
+        print(os.path.join(path, entry),pName)
+        if (os.path.isfile(os.path.join(path, entry)) and (pName in entry) and ('ebuild' in entry)) :
+            f = open(path+ '/' + entry, "r")
+            g = f.read()
+            print(g)
+            if "DESCRIPTION" in g:
+                print (g)
+                return g.split("DESCRIPTION: ")[1]
+    
+
+def populatePackages(c, portDir):
+    listOfPackages = []
+
+    # Create the packages table
+    c.execute("DROP TABLE IF EXISTS packages;")
+    c.execute("CREATE TABLE packages(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR, parent INTEGER, description VARCHAR);")
+
+    # put package groups into a list
+    c.execute("SELECT id,dir_name FROM tld;")
+    listOfCategories= c.fetchall() # gives a list of tuples
+
+    for category in listOfCategories:
+        
+        parentId = category[0] # first entry in tuple is id]
+        groupName = category[1] # last enttry is dir name
+
+        path = portDir + category[1] +'/'
+        
+        for entry in os.listdir(path):
+            if (os.path.isdir(os.path.join(path, entry))):
+                #listOfPackages.append(entry)
+                print("INSERT INTO packages(name, parent) VALUES(" + entry + ","+ str(parentId) + ");")
+                c.execute("INSERT INTO packages(name, parent) VALUES('" + entry + "',"+ str(parentId) + ");")
+            # 
+        conn.commit()
+
+    print(listOfPackages)
+
 if __name__ == "__main__":
     pw = PW.portageWalker()
     HOME = os.environ['HOME']+"/"
-    pwDict1 = pw.tldDict('en')
+    portageDirectory = "/usr/portage/"
+    packageGroupDict = pw.tldDict('en')
     conn = sqlite3.connect(HOME + 'portwalk.db')
     cur = conn.cursor()
-    populateTLD(cur, pwDict1)
+
+    # First - get the package groups
+    # and the descriptions
+    populateTLD(cur, packageGroupDict) 
+
+    # Now populate the packages table
+    populatePackages(cur,portageDirectory)
 
 
 
