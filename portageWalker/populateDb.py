3e5ba67469c3aad719df13983ace857a966b6d56
mule-ear <mark.winnemueller@codefutures.com>
2018-01-23 17:13:38 -0700

    fixed populateDb.py

--- portageWalker/populateDb.py
+++ portageWalker/populateDb.py
@@ -1,15 +1,9 @@
 import sqlite3, os
 import include.portageWalker as PW
 
-pw = PW.portageWalker()
-HOME = os.environ['HOME']+"/"
-pwDict = pw.tldDict('en')
-conn = sqlite3.connect(HOME + 'portwalk.db')
-cur = conn.cursor()
-
-def populatTLD(cur):
-    cur.execute("DROP TABLE IF EXISTS tld;")
-    cur.execute("CREATE TABLE tld(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dir_name VARCHAR , description STRING);")
+def populateTLD(c, pwDict):
+    c.execute("DROP TABLE IF EXISTS tld;")
+    c.execute("CREATE TABLE tld(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, dir_name VARCHAR , description STRING);")
 
     for key in pwDict:
         desc = pwDict[key]
@@ -17,12 +11,17 @@ def populatTLD(cur):
             desc = desc.split("'")[0] + chr(8217) + desc.split("'")[1]
         stmnt = "INSERT INTO tld(dir_name, description) VALUES('"+ key +"', '"+ desc+"');"
         # debug print(stmnt)
-        cur.execute(stmnt)
+        c.execute(stmnt)
 
     conn.commit()
 
 if __name__ == "__main__":
-    pass
+    pw = PW.portageWalker()
+    HOME = os.environ['HOME']+"/"
+    pwDict1 = pw.tldDict('en')
+    conn = sqlite3.connect(HOME + 'portwalk.db')
+    cur = conn.cursor()
+    populateTLD(cur, pwDict1)
 
 
 
